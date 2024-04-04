e5c73d63435d18c4888d1f7a77b05250fc0c05d0
taransriv10 <taransriv10@gmail.com>
2024-04-03 20:15:24 -0400

    tests added

--- src/test/java/com/example/dle/DleApplicationTests.java
+++ src/test/java/com/example/dle/DleApplicationTests.java
@@ -2,12 +2,34 @@ package com.example.dle;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
-@SpringBootTest
+import static org.assertj.core.api.ClassBasedNavigableIterableAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DleApplicationTests {
 
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
     @Test
-    void contextLoads() {
+    public void testDoubleNumberEndpoint() {
+        // Given
+        int inputNumber = 5;
+
+        // When
+        int result = restTemplate.getForObject("http://localhost:" + port + "/double?number=" + inputNumber, Integer.class);
+
+        // Then
+        assertEquals(inputNumber,result);
     }
 
 }
